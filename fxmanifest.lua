dda898c82ceb2ebfb12c2ecbc545dc28d7ddfb60
GESUS <105702323+DevAlexandre0@users.noreply.github.com>
2025-09-19 16:59:57 +0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 name 'esx_garage'
-description 'ESX Garage with ox_lib/ox_target/oxmysql/ox_inventory'
+description 'Global ESX Garage with zones/points, impound fixed price, active map, contract, blips'
 version '0.2.0'
 lua54 'yes'
 
@@ -13,18 +13,18 @@ shared_scripts {
 }
 
 client_scripts {
-  'client/*.lua'
+  'client/main.lua',
+  'client/blips.lua'
 }
 
 server_scripts {
   '@oxmysql/lib/MySQL.lua',
-  'server/*.lua'
+  'server/main.lua',
+  'server/contract.lua'
 }
 
 dependencies {
   'es_extended',
   'ox_lib',
-  'oxmysql',
-  'ox_target',
-  'ox_inventory'
+  'oxmysql'
 }
