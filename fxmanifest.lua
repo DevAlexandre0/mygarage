35886655588803bf324faa64e80fe6ceb6357048
GESUS <105702323+DevAlexandre0@users.noreply.github.com>
2025-09-19 16:15:19 +0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,31 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'esx_garage'
+description 'ESX Garage with ox_lib/ox_target/oxmysql'
+version '0.1.0'
+
+lua54 'yes'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua',
+    'shared/*.lua'
+}
+
+client_scripts {
+    'client/*.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/*.lua'
+}
+
+dependencies {
+    'es_extended',
+    'ox_lib',
+    'oxmysql'
+    -- 'ox_target', -- แนะนำ
+    -- 'ox_inventory' -- ทางเลือก
+}
